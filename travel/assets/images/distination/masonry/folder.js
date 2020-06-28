e65b214abcc1adaa634a9c89038cdf74691f35d7
Your Name <you@example.com>
2020-06-29 00:57:38 +0200

    Auto-generated commit

---
+++ travel/assets/images/distination/masonry/folder.js
@@ -0,0 +1,9 @@
+import * as $  from 'jquery';
+
+$('.grid').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  })
